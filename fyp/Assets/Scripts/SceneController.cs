55c67e20e152e43a71b857ae7e8bb5e119f1f451
AdkhamovaMunissa <90247211+AdkhamovaMunissa@users.noreply.github.com>
2023-10-12 20:44:57 +0800

    Created transition from intro to round2

---
+++ fyp/Assets/Scripts/SceneController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public Canvas introScene;
+    public Canvas round2Scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // introScene = GameObject.Find("Intro");
+        // round2Scene = GameObject.Find("Round2");
+
+        // SetObjectsActive(false, round2Scene);
+        // SetObjectsActive(true, introScene);
+        // introScene.enabled = true;
+        // round2Scene.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // private void SetObjectsActive(Canvas canvas)
+    // {
+    //     gameObject.SetActive(activeState);
+    // }
+
+    public void Round2Scene()
+    {
+        SceneManager.LoadScene("Round2-detrimental");
+        
+    }
+}
