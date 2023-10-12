34bd491cbe2f48e7f6c763d11d5f589c3bf8762a
AdkhamovaMunissa <90247211+AdkhamovaMunissa@users.noreply.github.com>
2023-10-12 16:57:01 +0800

    Fixed DialogueController bug

--- fyp/Assets/DialogueController.cs
+++ fyp/Assets/DialogueController.cs
@@ -11,11 +11,16 @@ public class DialogueController : MonoBehaviour
     public string[] Sentences;  // making this string so we have multiple sentences in the dialogue
     private int Index = 0;
     public float DialogueSpeed;
+    bool nextMessageCoroutine = false;
 
     
     void Update()
     {
-         if(Input.GetKeyDown(KeyCode.Space))
+        if(nextMessageCoroutine){
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && !nextMessageCoroutine)
         {   
             NextSentence();
         }
@@ -27,17 +32,22 @@ public class DialogueController : MonoBehaviour
         if(Index <= Sentences.Length - 1)
         {
             DialogueText.text = "";
+            
             StartCoroutine(WriteSentence());
+           
         }
     }
 
     IEnumerator WriteSentence()
     {
-        foreach(char Charachter in Sentences[Index].ToCharArray())
-        {
-            DialogueText.text += Charachter;
-            yield return new WaitForSeconds(DialogueSpeed);
-        }
-        Index++;
+        nextMessageCoroutine = true;
+            foreach(char Charachter in Sentences[Index].ToCharArray())
+            {
+            
+                DialogueText.text += Charachter;
+                yield return new WaitForSeconds(DialogueSpeed);
+            }
+            Index++;
+        nextMessageCoroutine = false;
     }
 }
