043b14f0da7f1b5e5d150cf9ab7997c352c76224
karenajoseph26 <79834410+karenajoseph26@users.noreply.github.com>
2023-10-12 11:53:56 +0800

    without animator

--- fyp/Assets/DialogueController.cs
+++ fyp/Assets/DialogueController.cs
@@ -3,32 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
+
 public class DialogueController : MonoBehaviour
 {
     public TextMeshProUGUI DialogueText;    //reference to the text
     public string[] Sentences;  // making this string so we have multiple sentences in the dialogue
     private int Index = 0;
     public float DialogueSpeed;
-    public Animator DialogueAnimator;
-    private bool StartDialogue = true;
 
-
+    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+         if(Input.GetKeyDown(KeyCode.Space))
         {   
-            if (StartDialogue)
-            {
-                DialogueAnimator.SetTrigger("Enter");
-                StartDialogue = false;
-            }
-            else
-            {
-                NextSentence();
-            }
-        }     
-    }
+            NextSentence();
+        }
 
+    }
 
     void NextSentence()
     {
@@ -37,15 +29,7 @@ public class DialogueController : MonoBehaviour
             DialogueText.text = "";
             StartCoroutine(WriteSentence());
         }
-        else
-        {
-            DialogueText.text = "";
-            DialogueAnimator.SetTrigger("Exit");
-            Index = 0;
-            StartDialogue = true;
-        }
     }
-
 
     IEnumerator WriteSentence()
     {
@@ -56,6 +40,4 @@ public class DialogueController : MonoBehaviour
         }
         Index++;
     }
-
-
 }
