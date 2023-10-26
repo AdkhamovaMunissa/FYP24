0763c1c2640cdb219a74fdce511069d8eb72135f
AdkhamovaMunissa <90247211+AdkhamovaMunissa@users.noreply.github.com>
2023-10-26 21:26:09 +0800

    Added question function

---
+++ fyp/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="QuizQuestion", fileName="New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter a new question here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswer;
+
+    public string GetQuestion() {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswer;
+    }
+
+    public string GetAnswer(int index) {
+        return answers[index];
+    }
+    
+}
---
+++ fyp/Assets/Scripts/Quiz.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerBtns;
+    int correctAnsIndex;
+    [SerializeField] Sprite defaultAnsSprite;
+    //default anssprite color: #09FFA0 
+    [SerializeField] Sprite correctAnsSprite;
+
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerBtns.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerBtns[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image btnImage;
+
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            btnImage = answerBtns[index].GetComponent<Image>();
+            btnImage.sprite = correctAnsSprite;
+            btnImage.color = Color.white;
+        }
+        else
+        {
+            correctAnsIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnsIndex);
+            questionText.text = "Sorry, the correct answer was:\n" + correctAnswer;
+            btnImage = answerBtns[correctAnsIndex].GetComponent<Image>();
+            btnImage.sprite = correctAnsSprite;
+            btnImage.color = Color.white;
+        }
+    }
+
+
+}
