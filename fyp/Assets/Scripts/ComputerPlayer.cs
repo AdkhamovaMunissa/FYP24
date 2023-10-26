9754dae87a634ef0d8399a20f2e0c7402bbc54c0
AdkhamovaMunissa <90247211+AdkhamovaMunissa@users.noreply.github.com>
2023-10-26 17:24:46 +0800

    Update

--- fyp/Assets/Scripts/ComputerPlayer.cs
+++ fyp/Assets/Scripts/ComputerPlayer.cs
@@ -9,7 +9,7 @@ public class ComputerPlayer : MonoBehaviour
     public Sprite crossImage;
     public Sprite tickImage;
     public int counter = 0;
-
+    public int[] cellValues;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,18 +33,40 @@ public class ComputerPlayer : MonoBehaviour
         int index = -1;
         while(!placed && counter < 8)
         {
-            index = Random.Range(0, 9);
+            // index = Random.Range(0, 9);
+            index = GetMoveIndex();
             if(!boxes[index].enabled)
             {
                 boxes[index].enabled = true;
                 boxes[index].sprite = crossImage;
+                cellValues[index] = 0;
                 placed = true;
                 counter++;
-                Debug.Log("Computer player: " + counter);
+                Debug.Log("Computer player: " + index);
             }    
         }
         
         
+    }
+
+    int GetMoveIndex()
+    {
+        for(int i=0; i<9; i++)
+        {
+
+        }
+        for(int i=0; i<9; i++)
+        {
+            if(!boxes[i].enabled)
+            {
+                return i;
+            }
+            else if(ReferenceEquals(boxes[i].sprite, tickImage))
+            {
+                Debug.Log("Tick is here");
+            }
+        }
+        return 9;
     }
 
     // private void OnMouseDown() {
