d8ad024b165ea9e0e62887acb514e03604c58a20
AdkhamovaMunissa <90247211+AdkhamovaMunissa@users.noreply.github.com>
2024-03-12 23:53:52 +0800

    Updated the size of the scene

--- fyp/Assets/Scripts2/FollowCamera.cs
+++ fyp/Assets/Scripts2/FollowCamera.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] GameObject thingToFollow;
-    // this things position (camera) should be the same as the car's position
+    // [SerializeField] GameObject thingToFollow;
+    // // this things position (camera) should be the same as the car's position
 
-    void LateUpdate()
+    // void LateUpdate()
+    // {
+    //     transform.position = thingToFollow.transform.position + new Vector3 (0,0,-10);
+    // }
+
+    [SerializeField] private GameObject thingToFollow;
+    [SerializeField] private GameObject minXAndYObject;
+    [SerializeField] private GameObject maxXAndYObject;
+
+    public Vector2 minXAndY;
+    public Vector2 maxXAndY;
+
+    private void Start()
     {
-        transform.position = thingToFollow.transform.position + new Vector3 (0,0,-10);
+        minXAndY = minXAndYObject.transform.position;
+        maxXAndY = maxXAndYObject.transform.position;
     }
 
+    private void LateUpdate()
+    {
+        Vector3 newPosition = thingToFollow.transform.position + new Vector3(0, 0, -10);
+        newPosition.x = Mathf.Clamp(newPosition.x, minXAndY.x, maxXAndY.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minXAndY.y, maxXAndY.y);
+
+        transform.position = newPosition;
+    }
 }
