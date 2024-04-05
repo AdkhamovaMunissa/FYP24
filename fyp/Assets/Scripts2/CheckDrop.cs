1ed2cf89b105f3efdb6d05ab1e7ee1e966e4cd2e
AdkhamovaMunissa <90247211+AdkhamovaMunissa@users.noreply.github.com>
2024-04-05 23:52:18 +0800

    Created dragNdrop

--- fyp/Assets/Scripts2/CheckDrop.cs
+++ fyp/Assets/Scripts2/CheckDrop.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class CheckDrop : MonoBehaviour, IDropHandler
 {
     public GameObject correctObject; // The correct object to be dropped on the slot
     public bool OnDropSuccess = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI WarningText;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        WarningText.gameObject.SetActive(false);
+        WarningText.transform.parent.gameObject.SetActive(false);
+
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -25,19 +24,30 @@ public class CheckDrop : MonoBehaviour, IDropHandler
         Debug.Log("OnDrop");
         if (eventData.pointerDrag != null)
         {
-            // Check if the correct object is dropped on the slot
-            if (eventData.pointerDrag.gameObject == correctObject)
+            if (eventData.pointerDrag == correctObject)
             {
-                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+               eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                Debug.Log("Correct object");
                 OnDropSuccess = true;
-                Debug.Log("Drop Successfull");
+
+                WarningText.gameObject.SetActive(true);
+                WarningText.transform.parent.gameObject.SetActive(true);
+                WarningText.text = "Excellent. It is correct!";
+
+                eventData.pointerDrag = null;
+                return; 
             }
-            else{
-                OnDropSuccess = false;
-                Debug.Log("Drop Fail");
+            else 
+            {
+                Debug.Log("Not the correct object");
+
+                WarningText.gameObject.SetActive(true);
+                WarningText.transform.parent.gameObject.SetActive(true);
+                WarningText.text = "Oops! Wrong equipment...";
             }
-            
+             
         }
+        
     }
 
 }
